d0bc3088cbcf2068538455ade36560cd6e766c97
Kelson Ball <kelson.j.ball@gmail.com>
2018-11-08 07:21:39 -0800

    add dotnet install script

---
+++ scripts_install/setup.sh
@@ -0,0 +1,7 @@
+# Install dotnet core 2.1 on Ubuntu 18.10
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+
+apt-get install apt-transport-https
+apt-get update
+apt-get install dotnet-sdk-2.1
